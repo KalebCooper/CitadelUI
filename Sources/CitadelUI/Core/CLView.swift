170d150ad38208ad96b9347c6f6f3b9ef716f421
Kaleb Cooper <thekalebcooper@gmail.com>
2020-11-22 20:27:20 -0600

    CLView commit

---
+++ Sources/CitadelUI/Core/CLView.swift
@@ -0,0 +1,95 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kaleb Cooper on 11/22/20.
+//
+
+//
+//  UIView+Mode.swift
+//  slym-ios
+//
+//  Created by Kaleb Cooper on 7/2/20.
+//  Copyright © 2020 Cooper Labs LLC. All rights reserved.
+//
+
+import UIKit
+
+public protocol ViewConfigurable {
+    
+}
+public enum ViewConfigureType {
+    case setup
+    case update
+}
+public enum ViewState {
+    case empty
+    case loading
+    case done
+}
+protocol CLViewStateDelegate: class {
+    func view(updatedTo state: ViewState)
+}
+public class CLView: UIView {
+    
+    weak var stateDelegate: CLViewStateDelegate?
+    
+    //----------------------------------------------------------------
+    // MARK: - Convenience Values
+    //----------------------------------------------------------------
+    
+    
+    
+    //----------------------------------------------------------------
+    // MARK: - UI Constraint Convenience
+    //----------------------------------------------------------------
+    
+    // MARK: Safe Area
+    
+    ///Convenience shorthand for `safeAreaLayoutGuide.topAnchor`
+    private(set) lazy var topSafe: NSLayoutYAxisAnchor = {
+        return safeAreaLayoutGuide.topAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.leftAnchor`
+    private(set) lazy var leftSafe: NSLayoutXAxisAnchor = {
+        return safeAreaLayoutGuide.leftAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.rightAnchor`
+    private(set) lazy var rightSafe: NSLayoutXAxisAnchor = {
+        return safeAreaLayoutGuide.rightAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.leadingAnchor`
+    private(set) lazy var leadingSafe: NSLayoutXAxisAnchor = {
+        return safeAreaLayoutGuide.leadingAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.trailingAnchor`
+    private(set) lazy var trailingSafe: NSLayoutXAxisAnchor = {
+        return safeAreaLayoutGuide.trailingAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.bottomAnchor`
+    private(set) lazy var bottomSafe: NSLayoutYAxisAnchor = {
+        return safeAreaLayoutGuide.bottomAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.centerXAnchor`
+    private(set) lazy var centerXSafe: NSLayoutXAxisAnchor = {
+        return safeAreaLayoutGuide.centerXAnchor
+    }()
+    ///Convenience shorthand for `safeAreaLayoutGuide.centerYAnchor`
+    private(set) lazy var centerYSafe: NSLayoutYAxisAnchor = {
+        return safeAreaLayoutGuide.centerYAnchor
+    }()
+    
+    var state: ViewState! {
+        didSet {
+            stateDelegate?.view(updatedTo: state)
+        }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
